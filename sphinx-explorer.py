b52dabdabbe594fb21f215f6afb69418ce644c7e
Toshiyuki Ishii <pashango2@gmail.com>
2017-03-29 11:25:29 +0900

    fix pyinstaller error

--- sphinx-explorer.py
+++ sphinx-explorer.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, absolute_import, unicode_literals
 from sphinx_explorer import main
+import mdx_gfm
 
 
 if __name__ == "__main__":
